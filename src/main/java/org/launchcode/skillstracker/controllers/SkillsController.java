74ada36b2a9b9ce729ff812db1c7cdc92ae4e7db
i-swa <ievaswanson@u.boisestate.edu>
2024-04-25 20:49:22 -0500

    part 1 completed

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -1,4 +1,38 @@
 package org.launchcode.skillstracker.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class SkillsController {
+    // Responds to get and post requests at /hello?coder=LaunchCoder
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String hello(@RequestParam String coder) {
+        return "Hello, " + coder + "!";
+    }
+
+    @GetMapping
+    @ResponseBody
+    public String helloForm() {
+        String html =
+                "<html>" +
+                        "<body>" +
+                        "<h1>Skills Tracker<h1>" +
+                        "<h2>We have a few skills we would like to learn. Here's the list!" +
+                        "<ol>" +
+                            "<li>Java</li>" +
+                            "<li>JavaScript</li>" +
+                            "<li>Python</li>" +
+                        "</ol>" +
+                        "<form method = 'post' action = '/hello'>" +
+                        "<input type = 'text' name = 'coder' />" +
+
+                        "</div>"
+                        "<input type = 'submit' value = 'Submit' />" +
+                        "</form>" +
+                        "</body>" +
+                        "</html>";
+        return html;
+    }
 }
